223355806e5f094abfc58a86f3fdb291547b5540
yego0210 <diegotrujillo0210@gmail.com>
2022-04-24 22:41:25 -0500

    Django

---
+++ Ejemplo.py
@@ -0,0 +1,15 @@
+
+# TODO:  Comandos para inicar un proyecto en Django por pimera ves
+
+
+#  1 ) = 	python -m venv nombredelEntono,      # TODO: creo el entorno
+
+#  2 ) =    venv\Scripts\activate              #   TODO: activar el entorno &&   // TODO: " deactivate " Es para desactivar el entorno
+
+#  3 ) =    pip install Django                 #   TODO:  install Django
+
+#  4 ) =    django-admin startproject nombreDelProyecto   # TODO: Inciar proyecto de Django
+
+#  5) = 	python manage.py runserver    # TODO:  correr la aplicacion  iniciar ala palicacion
+
+
